a694f4aa3d1c517b2eba3d232dd743f5172c04c3
육주용 <yookjy@tmon.co.kr>
2021-10-14 14:02:20 +0900

    샘플 수정

--- example/example.dart
+++ example/example.dart
@@ -1,32 +1,10 @@
-library test;
-
-
 import 'package:action_box_generator/builder.dart';
-
+//add
 import 'example.config.dart';
 
-
-@ActionCenterConfig(actionCenterTypeName: 'SpcActionCenter', actionRootTypeName: 'ActionRoot', generateForDir: ['*'])
+@ActionCenterConfig(
+  actionCenterTypeName: 'SpcActionCenter',
+  actionRootTypeName: 'ActionRoot',
+  generateForDir: ['*']
+)
 final SpcActionCenter actionCenter = SpcActionCenter();
-
-
-// class ActionBuilder<TParam, TResult, TAction extends Action<TParam, TResult>> extends StreamBuilder<TResult> {
-//   ActionBuilder({
-//     Key? key,
-//     required ActionDescriptor<TAction, TParam, TResult> Function(ActionGroup set) actionChooser, //ActionGroup 제너레이트
-//     required AsyncWidgetBuilder<TResult> builder,
-//     TResult? initialData,
-//     Channel Function(TAction)? channelChooser,
-//     Stream<TResult>? Function(Stream<TResult>)? streamHandler,
-//   }) : super(
-//     key: key,
-//     initialData: initialData,
-//     stream: SpcActionCenter.getStreamForSubscription(
-//       actionChooser: actionChooser,
-//       channelChooser: channelChooser,
-//       streamHandler: streamHandler
-//     ),
-//     builder : builder,
-//   );
-//
-// }
